a7de3d156c29374d2c248850873e758798704fbe
phillip stanley <phillip.stanley39@gmail.com>
2023-10-06 22:35:47 +0100

    feat: impletmented website menu
    feat: added menu and mobile version (no-js)

--- planets/urls.py
+++ planets/urls.py
@@ -1,5 +1,9 @@
+from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from . import views
 
 if settings.DEBUG:
-    urlpatterns = [] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns = [
+        path('', views.planet_detail, name='planet_detail'),
+    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- planets/views.py
+++ planets/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Planet
 
-# Create your views here.
+
+def planet_detail(request, name='Mercury'):
+    planet = get_object_or_404(
+        Planet,
+        name=name,
+        status=Planet.Status.PUBLISHED,
+    )
+
+    return render(
+        request,
+        'planets/planet_detail.html',
+        {
+            'planet': planet
+        })
